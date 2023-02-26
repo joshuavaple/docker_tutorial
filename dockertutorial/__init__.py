a0c08bae81e05ce4a81093044be946c1da6b5525
phuonglevu3112@gmail.com <phuonglevu3112@gmail.com>
2023-02-26 17:19:09 +0800

    added due to renaming project package

---
+++ dockertutorial/__init__.py
@@ -0,0 +1,1 @@
+print("import src parent package")
---
+++ dockertutorial/package1/module1.py
@@ -0,0 +1,5 @@
+def print_hello_1(name):
+    print(f"Hello {name}, this is module1")
+
+data_dict = {"name": ["Joshua", "Teresa"],
+             "age": [30, 27]}
