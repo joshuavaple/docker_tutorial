1fdb26d49384658150c09366afb23565efdb8f29
phuonglevu3112@gmail.com <phuonglevu3112@gmail.com>
2023-02-26 17:18:35 +0800

    deleted due to renaming project package from src to dockertutorial

--- src/package1/module1.py
+++
@@ -1,5 +0,0 @@
-def print_hello_1(name):
-    print(f"Hello {name}, this is module1")
-
-data_dict = {"name": ["Joshua", "Teresa"],
-             "age": [30, 27]}
--- src/package2/module2.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-def print_hello_2(name):
-    print(f"Hello {name}, this is module2")
-
-def make_dataframe(data_dict: dict):
-    return pd.DataFrame(data_dict)
